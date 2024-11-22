a4b2bb2030dd1fb91b52dbda3eaecdb955cebb5f
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-11-22 15:09:36 +0100

    ludde-help

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 const Header: React.FC = () => {
     return (
-        <header className="bg bg-slate-300 py-3">
+        <header className="py-3">
             <div className="container mx-auto px-4 flex items-center ">
                 <div className="logoImage">
                     <img src="src\assets\images\tempLogo.svg" alt="Kiosk Portalen"></img>
                 </div>
                 
-                    <h1 className="w-full mx-auto text-center text-2xl font-bold">Välkommen till Kiosk Portalen</h1>
+                    
                 
             </div>
         </header>
--- src/layouts/NavLayout.tsx
+++ src/layouts/NavLayout.tsx
@@ -1,12 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
 
 function NavLayout() {
-    return (
-        <>
-            <div>NavLayout</div>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <div className="wrapper">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default NavLayout
+export default NavLayout;
