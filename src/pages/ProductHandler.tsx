3574541dd77acd5875a5992e6b719ec57c1a84fe
Ludvig <ludvig.vinoy@hotmail.com>
2024-11-25 09:20:13 +0100

    producthandler started

--- src/pages/ProductHandler.tsx
+++ src/pages/ProductHandler.tsx
@@ -7,7 +7,7 @@ function ProductHandler() {
   return (
     <section>
       <div className="container mx-auto px-4 flex-row items-center">
-        <h2 className="font-bold text-xl my-4">Produkthantering</h2>
+        <h2 className="font-bold text-4xl my-4">Produkthantering</h2>
 
     <button className="rounded-xl border bg-card text-card-foreground shadow aspect-video h-32 flex p-3">
       <h3 className="font-extrabold text-left">Lägg till produkt</h3>
