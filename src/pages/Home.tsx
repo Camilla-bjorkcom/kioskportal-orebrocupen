9841ae822b7b47f9988bfe55d4ca4bdedd7d9ebe
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-11-25 13:32:06 +0100

    Dasboard page, responsive design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import TournamentBtn from "@/components/TournamentBtn"
 
 function Home() {
   return (
-    <div className="container mx-auto ">
+    <div className="container mx-auto">
       <h1 className="font-bold text-5xl mb-24">Välkommen tillbaka</h1>
       <CreateTournamentBtn />
       <p className="pt-64 font-bold">Avslutade turneringar</p>
