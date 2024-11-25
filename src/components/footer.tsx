4f9aa409553cb1bc18a73a434e75e0811095f50c
Andreas Fredriksson <afredriksson89@gmail.com>
2024-11-25 09:52:01 +0100

    footer title and datetime added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,14 @@
 const Footer: React.FC = () => {
+
+    const currentYear = new Date().getFullYear();
+    const footerTitle = "© Kiosk Portal";
+
     return (
         <footer className="bg">
             <div className="container mx-auto px-4 flex">
                
                 <div>
-                    <h1> © Kiosk Portal 2024-11-20</h1>
+                    <h1> {footerTitle} {currentYear}</h1>
                 </div>
             </div>
         </footer>
