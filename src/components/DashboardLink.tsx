41ff008a32d7d79fb0c15b8958c05ee19c99d321
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-12-02 14:50:48 +0100

    added buttons, deleted qr-link input field

--- src/components/DashboardLink.tsx
+++ src/components/DashboardLink.tsx
@@ -1,17 +1,12 @@
-import { CopyIcon } from "@radix-ui/react-icons";
-import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const DashboardLink = () => {
   return (
     <div className="mt-10 flex flex-col items-center md:items-start ">
-      <div className="flex w-1/2 justify-between ">
-        <h3 className="font-bold text-xl mt-10 mb-4">
-          Länk till kioskapplikation
-        </h3>
-        <CopyIcon className="w-8 h-8 place-self-center" />
-      </div>
-
-      <Input className="md:w-1/2 w-3/4" />
+      <div className="flex w-1/2 gap-5">
+      <Button>Inventera turneringsprodukter</Button>
+      <Button>Qr-koder</Button>
+        </div>
     </div>
   );
 };
