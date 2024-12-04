6fe61069d8ce0ba60a9d8775fccf8d9dffef2d1a
Ludvig <ludvig.vinoy@hotmail.com>
2024-12-04 16:28:15 +0100

    chgfgjf

--- src/components/HandleProductListButton.tsx
+++ src/components/HandleProductListButton.tsx
@@ -1,75 +1,180 @@
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@radix-ui/react-dialog'
 
-import { DialogHeader } from './ui/dialog'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
-import { Input } from './ui/input'
-import { ReaderIcon } from '@radix-ui/react-icons'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { ReaderIcon } from "@radix-ui/react-icons";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { PropsWithChildren, ReactNode, useState } from "react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@radix-ui/react-collapsible";
+import { useQuery } from "@tanstack/react-query";
+import { Checkbox } from "./ui/checkbox";
 
-
-const formSchema = z.object({
-    productlistname: z.string().min(2, {
-      message: "Produktlistnamn måste ha minst 2 bokstäver",
-    }),
-  })
-
-  
-
-
-function HandleProductListButton() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          productlistname: "",
-        },
-      });
-    
-      function onSubmit(values: z.infer<typeof formSchema>) {
-       
-        console.log(values);
-        form.reset();
-      }
-  return (
-    <Dialog>
-    <DialogTrigger asChild>
-      <button className="flex flex-col p-2 justify-between rounded-xl border-2 border-dashed bg-card text-card-foreground shadow hover:bg-slate-800 hover:text-white text-black aspect-video h-32">
-        <ReaderIcon className=" w-32 h-32 mx-auto" />
-        <p className="text-center w-full mb-4 ">Skapa Produkt</p>
-      </button>
-    </DialogTrigger>
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>Skapa Produkt</DialogTitle>
-        <DialogDescription className="sr-only">
-          Fyll i informationen för att skapa en ny Produkt</DialogDescription>
-      </DialogHeader>
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="productlistname"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Produktnamn</FormLabel>
-              <FormControl>
-                <Input placeholder="Skriv in produktnamn" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-       
-        <div className="flex justify-end">
-          <button type="submit" className=" border border-solid hover:bg-slate-800 hover:text-white rounded-xl p-2 mt-8 shadow">Spara Produkt</button>
-        </div>
-     
-      </form>
-      </Form>
-    </DialogContent>
-  </Dialog>
-)
+interface ProductList {
+  id: number;
+  productlistname: string;
+  products: [];
+}
+interface Product {
+  id: number;
+  productname: string;
 }
 
-export default HandleProductListButton
+const formSchema = z.object({
+  productlistname: z.string().min(2, {
+    message: "Produktlistnamn måste ha minst 2 bokstäver",
+  }),
+});
+
+function HandleProductListButton({ children }: PropsWithChildren) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      productlistname: "",
+    },
+  });
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
+
+  const UpdateProductListButton = async (
+    productlistname: string,
+    id: number
+  ) => {
+    try {
+      const response = await fetch("http://localhost:3000/productlists", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          productlistname: productlistname,
+          id: id,
+          products: selectedProducts,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("failed to update list");
+      }
+      const data = await response.json();
+      setSelectedProducts(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { isLoading, error } = useQuery<Product[]>({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3000/products");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      setProducts(data);
+      return data;
+    },
+  });
+
+  const handleSubmit = form.handleSubmit((values) => {
+    console.log(values);
+    form.reset();
+  });
+
+  if (isLoading) {
+    return <div>Loading products...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {String(error)}</div>;
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="flex flex-col p-2 justify-between bg-card text-card-foreground hover:text-white text-black aspect-video h-32">
+          {children}
+          <ReaderIcon className=" w-32 h-32 mx-auto" />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Skapa Produkt</DialogTitle>
+          <DialogDescription className="sr-only">
+            Fyll i informationen för att skapa en ny Produkt
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="productlistname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Produktlistnamn</FormLabel>
+                  <FormControl>
+                    <Input placeholder="skriv in produktlistnamn" {...field} />
+                  </FormControl>
+                  <Collapsible>
+                    <CollapsibleTrigger>
+                      Lägg till produkter i din lista
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <div className="mt-4 grid-cols-3 gap-2">
+                        {products.map((product) => (
+                          <div
+                            key={product.id}
+                            className="flex items-center space-x-2"
+                          >
+                            <Checkbox
+                              id={`product-${product.id}`}
+                              checked={selectedProducts.includes(product.id)}
+                              onCheckedChange={(checked) => {
+                                if (checked) {
+                                  setSelectedProducts((prev) => [
+                                    ...prev,
+                                    product.id,
+                                  ]);
+                                } else {
+                                  setSelectedProducts((prev) =>
+                                    prev.filter((id) => id !== product.id)
+                                  );
+                                }
+                              }}
+                            />
+                            <label
+                              htmlFor={`product-${product.id}`}
+                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              {product.productname}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                    </CollapsibleContent>
+                  </Collapsible>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default HandleProductListButton;
--- src/pages/ProductListHandler.tsx
+++ src/pages/ProductListHandler.tsx
@@ -1,6 +1,7 @@
 
 
 import CreateProductListButton from "@/components/CreateProductListButton";
+import HandleProductListButton from "@/components/HandleProductListButton";
 import PagesHeader from "@/components/PagesHeader";
 import { TrashIcon } from "@radix-ui/react-icons";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
@@ -36,7 +37,7 @@ function ProductListHandler() {
   });
 
   // Spara ny produktlista (POST)
-  const handleSaveProductList = async (productListName: string) => {
+  const SaveProductList = async (productListName: string) => {
     try {
       const response = await fetch('http://localhost:3000/productslists', {
         method: "POST",
@@ -54,7 +55,7 @@ function ProductListHandler() {
   };
 
   // Ta bort produktlista (DELETE)
-  const handleDeleteProductsList = async (id: number) => {
+  const DeleteProductsList = async (id: number) => {
     try {
       const response = await fetch(
         `http://localhost:3000/productslists/${id}`,
@@ -66,13 +67,14 @@ function ProductListHandler() {
         throw new Error("Failed to delete product list");
       }
       setProductLists((prev) => prev.filter((list) => list.id !== id)); // Uppdatera state lokalt
-    } catch (error) {
+    } 
+    catch (error) {
       console.error(error);
     }
   };
 
   if (isLoading) {
-    return <div>Loading product lists...</div>;
+    return <div>Loading productlists...</div>;
   }
 
   if (error) {
@@ -82,25 +84,25 @@ function ProductListHandler() {
     <section>
       <PagesHeader pathname={pathname} />
       <div className="container mx-auto px-4 flex-row items-center">
-        <h2 className="font-bold text-4xl my-4">Produkthantering</h2>
+        <h2 className="font-bold text-4xl my-4">Produktlistor</h2>
 
-        <CreateProductListButton onSave={handleSaveProductList} />
-    <div className="mt-8">
+        <CreateProductListButton onSave={SaveProductList} />
+      <div className="mt-8">
           <h3 className="font-semibold text-2xl">Sparade produktlistor:</h3>
           <div className="mt-4 flex gap-2">
           {productlists.map((productlist) =>  (
-              <button>
+              <HandleProductListButton key={productlist.id}>
               <div
-                key={productlist.id}
+                
                 className="flex flex-col p-2 justify-between rounded-xl border-2
                  bg-card text-card-foreground shadow hover:bg-slate-800 hover:text-white text-black aspect-video h-32">
               <div className="flex justify-between">
                 <p className="font-semibold text-2xl flex">{productlist.productlistname}</p>
-                <button onClick={() => handleDeleteProductsList(productlist.id)}><TrashIcon className="w-8 h-6  hover:text-red-500 "></TrashIcon></button>
+                <button onClick={() => DeleteProductsList(productlist.id)}><TrashIcon className="w-8 h-6  hover:text-red-500 "></TrashIcon></button>
                 </div>
-                <p className="text-right">Lägg till produkter</p>
+                
               </div>
-              </button>
+              </HandleProductListButton>
             ))}
           </div>
         </div>
