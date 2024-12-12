357f0fa80dfe0ab0ce68fedf1d0d0f8e80a211e4
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2024-12-12 08:13:26 +0100

    add kiosk, get productlist in json-server. commented add product

--- src/pages/Kioskmanager.tsx
+++ src/pages/Kioskmanager.tsx
@@ -3,7 +3,7 @@ import AddKioskButton from "@/components/AddKioskButton";
 import { useState } from "react";
 import { TrashIcon } from "lucide-react";
 import AddProductListButton from "@/components/AddProductListButton";
-import AddProductsButton from "@/components/AddProductButton";
+// import AddProductsButton from "@/components/AddProductButton";
 import { useQuery } from "@tanstack/react-query";
 
 interface Facility {
@@ -15,32 +15,35 @@ interface Kiosk {
   kioskName: string;
 }
 
+interface ProductList {
+  id: number;
+  productlistname: string;
+  products: Product[];
+}
+interface Product {
+  id: number;
+  productname: string;
+}
+
 function Kioskmanager() {
-  type ProductListItem = {
-    productListName: string;
-    products: string[];
-  };
-
- 
-
   const [facility, setFacility] = useState<Facility[]>([]);
   const [kiosks, setKiosks] = useState<Kiosk[]>([]);
   const [selectedFacility, setSelectedFacility] = useState<number | null>(null);
   const [selectedKiosk, setSelectedKiosk] = useState<number | null>(null);
-  const [productList, setProductList] = useState<ProductListItem | undefined>();
-  const [products, setProducts] = useState<string[]>([]);
+  const [productList, setProductList] = useState<ProductList | undefined>();
+  // const [products, setProducts] = useState<string[]>([]);
 
   //Sparar ned vad användaren valt för värden i UI i selectedOptions, ska ändras från string till id sen och skickas till databas för put och get
   const [selectedOptions, setSelectedOptions] = useState<{
     facility: number | undefined;
     kiosk: number | null;
-    productlist: string | undefined;
+    productlist: number | undefined;
   }>({
     facility: undefined,
     kiosk: null,
     productlist: undefined,
   });
- 
+
   useQuery<Facility[]>({
     queryKey: ["facilities"],
     queryFn: async () => {
@@ -90,7 +93,7 @@ function Kioskmanager() {
       const response = await fetch("http://localhost:3000/kiosks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ kiosks: kioskName }),
+        body: JSON.stringify({ kioskName: kioskName }),
       });
       if (!response.ok) {
         throw new Error("Failed to save product");
@@ -103,41 +106,44 @@ function Kioskmanager() {
     }
   };
 
-
-  const addProductList = (productList: ProductListItem | undefined) => {
+  const addProductList = (productList: ProductList | undefined) => {
     setProductList(productList);
     if (productList != undefined) {
       setSelectedOptions((prev) => ({
         ...prev,
-        productlist: productList.productListName,
+        productlist: productList.id,
       }));
     }
   };
 
-  const addProduct = (productName: string) => {
-    setProducts((prev) => [...prev, productName]);
-  };
+  // const addProduct = (productName: string) => {
+  //   setProducts((prev) => [...prev, productName]);
+  // };
 
   const handleFacilityClick = (facility: Facility) => {
-    setSelectedFacility(facility.id);
-
-    //återställ kiosk
+    setSelectedFacility((prevSelectedFacility) =>
+      prevSelectedFacility === facility.id ? null : facility.id
+    );
+  
+    // Återställ kiosk och uppdatera valda alternativ
     setSelectedKiosk(null);
-
-    setSelectedOptions({
-      facility: facility.id,
-      kiosk: null,
-      productlist: undefined,
-    });
-  };
-
-  const handleKioskClick = (kiosk: Kiosk) => {
-    setSelectedKiosk(kiosk.id);
-
-    // Uppdatera `selectedOptions`
     setSelectedOptions((prev) => ({
       ...prev,
-      kiosk: kiosk.id,
+      facility: selectedFacility === facility.id ? undefined : facility.id,
+      kiosk: null,
+      productlist: undefined,
+    }));
+  };
+  
+  const handleKioskClick = (kiosk: Kiosk) => {
+    setSelectedKiosk((prevSelectedKiosk) =>
+      prevSelectedKiosk === kiosk.id ? null : kiosk.id
+    );
+  
+    // Uppdatera valda alternativ
+    setSelectedOptions((prev) => ({
+      ...prev,
+      kiosk: selectedKiosk === kiosk.id ? null : kiosk.id,
       productlist: prev.productlist,
     }));
   };
@@ -156,15 +162,24 @@ function Kioskmanager() {
     }
   };
 
-  // const removeFacility = (facilityId: string) => {
-  // };
-  // const removeKiosk = (kioskId: string) => {
-  // };
+  const DeleteKiosk = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:3000/kiosks/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("failed to delete product");
+      }
+      setKiosks((prev) => prev.filter((list) => list.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   console.log(selectedOptions);
+
   return (
     <>
-
       <section className="container mx-auto px-5">
         <h1 className="text-3xl font-bold mb-10">Skapa kiosker och utbud</h1>
         <div className="grid lg:grid-cols-3 gap-5 w-10/12">
@@ -183,8 +198,7 @@ function Kioskmanager() {
                  }`}
                   onClick={() => handleFacilityClick(facility)}
                 >
-                  {facility.facilityname}{" "}
-                  {/* Lägg till removeFacility onClick */}
+                  {facility.facilityname}
                   <TrashIcon
                     onClick={() => DeleteFacility(facility.id)}
                     className="mr-5 w-5 h-5 place-self-center hover:text-red-500"
@@ -205,15 +219,15 @@ function Kioskmanager() {
                       key={index}
                       className={`ml-3 pl-3 cursor-pointer mb-2 flex justify-between 
                         ${
-                          selectedKiosk === index
+                          selectedKiosk === kiosk.id
                             ? "text-black border-black border rounded-xl h-fit w-11/12"
                             : "text-black border-none w-11/12"
                         }            
                 `}
                       onClick={() => handleKioskClick(kiosk)}
                     >
-                      {kiosk.kioskName} {/* Lägg till removeKiosk onClick på trashIcon */}
-                      <TrashIcon className="mr-7 w-5 h-5 place-self-center  hover:text-red-500" />
+                      {kiosk.kioskName}                 
+                      <TrashIcon onClick={() => DeleteKiosk(kiosk.id)} className="mr-7 w-5 h-5 place-self-center  hover:text-red-500" />
                     </p>
                   ))}
                 </div>
@@ -229,26 +243,28 @@ function Kioskmanager() {
                   {productList === undefined && (
                     <AddProductListButton onSave={addProductList} />
                   )}
+
                   <ul className="ml-5">
                     <div className="flex justify-between">
                       <h3 className="font-semibold">
-                        {productList?.productListName}
+                        {productList?.productlistname}
                       </h3>
                       {productList != undefined && (
-                        <TrashIcon className="mr-5 w-5 h-5 place-self-center cursor-pointer hover:text-red-500" />
+                        <TrashIcon  className="mr-5 w-5 h-5 place-self-center cursor-pointer hover:text-red-500" />
                       )}
                     </div>
                     {productList?.products.map((product, index) => (
                       <li key={index} className="list-inside ml-4">
-                        {product}
+                        {product.productname}
                       </li>
                     ))}
                   </ul>
+                 
+{/*                  
                   <ul>
                     {productList != undefined && (
                       <AddProductsButton onSave={addProduct} />
                     )}
-
                     {products.map((product, index) => (
                       <div className="flex justify-between">
                         <li key={index} className="list-inside ml-9">
@@ -257,7 +273,9 @@ function Kioskmanager() {
                         <TrashIcon className="mr-5 w-4 h-4 place-self-center cursor-pointer hover:text-red-500" />
                       </div>
                     ))}
-                  </ul>
+                  </ul> */}
+
+
                 </div>
               )}
             </div>
