f5a2923664da0111a83da663159f4111df1ea603
Andreas Fredriksson <afredriksson89@gmail.com>
2025-02-24 13:50:07 +0100

    Update layout and styling in FacilitiesAndKiosks component; change favicon and enhance button styles for better responsiveness

--- src/pages/FacilitiesAndKiosks.tsx
+++ src/pages/FacilitiesAndKiosks.tsx
@@ -139,8 +139,8 @@ function FacilitiesAndKiosks() {
 
   return (
     <section className="container mx-auto px-5">
-      <div className="flex justify-between items-center w-3/4">
-        <h1 className="mt-8 text-2xl pb-2 mb-4 ">Anläggningshantering</h1>
+      <div className="flex flex-col md:flex-row justify-between items-center w-full md:w-3/4">
+        <h1 className="mt-8 text-2xl pb-2 mb-4">Anläggningshantering</h1>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
@@ -152,9 +152,10 @@ function FacilitiesAndKiosks() {
           </Tooltip>
         </TooltipProvider>
       </div>
-      <div className="flex justify-between w-full 2xl:w-3/4 items-center mb-3">
+
+      <div className="flex flex-col gap-4 mt-4 sm:mt-0 md:flex-row sm:justify-between sm:items-center w-full md:w-3/4 mb-3">
         <AddFacilityButton tournamentId={tournamentId!} />
-        <div>
+        <div className="flex flex-col sm:flex-row gap-3 place-items-start ">
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger>
@@ -186,16 +187,17 @@ function FacilitiesAndKiosks() {
           </TooltipProvider>
         </div>
       </div>
+
       <Accordion
         type="multiple"
         value={openFacilityId ? [openFacilityId] : []}
-        className="w-full 2xl:w-3/4"
+        className="w-full md:w-3/4"
       >
         {data.map((facility) => (
           <AccordionItem
             key={facility.id}
             value={openFacilityId === facility.id ? facility.id : ""}
-            className="p-4 border border-gray-200 rounded-md shadow dark:bg-slate-800 dark:text-gray-200 dark:border-slate-500"
+            className="p-4 border border-gray-200 rounded-md shadow dark:bg-slate-800 dark:text-gray-200 dark:border-slate-500 mb-4"
           >
             <AccordionTrigger
               className="text-lg font-medium hover:no-underline mr-2"
@@ -215,7 +217,7 @@ function FacilitiesAndKiosks() {
                   facilityId={facility.id}
                   onFacilityAdded={setOpenFacilityId}
                 />
-                <div className="flex justify-self-end gap-7 2xl:gap-2 ml-auto mr-5 w-fit basis-1/12 ">
+                <div className="flex flex-wrap sm:justify-end gap-4 ml-auto mr-5 w-fit basis-1/12">
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger>
@@ -249,19 +251,19 @@ function FacilitiesAndKiosks() {
               </div>
             </AccordionTrigger>
             <AccordionContent>
-              <div className="p-4 ">
+              <div className="p-4">
                 <div>
                   {facility.kiosks?.map((kiosk) => (
                     <div
                       key={kiosk.id}
-                      className="p-4 border border-gray-200 rounded-md shadow dark:border-slate-500"
+                      className="p-4 border border-gray-200 rounded-md shadow dark:border-slate-500 mb-4"
                     >
-                      <div className="flex justify-between items-center">
+                      <div className="flex flex-col md:flex-row sm:justify-between sm:items-center ">
                         <p className="font-semibold text-lg">
                           {kiosk.kioskName}
                         </p>
 
-                        <div className="flex justify-self-end gap-7 2xl:gap-3 ml-auto w-fit items-center">
+                        <div className="flex flex-wrap sm:justify-end gap-4 ml-auto w-fit mt-2 md:mt-0 items-center">
                           <AddProductsToKioskButton
                             kioskForEdit={kiosk}
                             productlists={productlists || []}
@@ -318,7 +320,7 @@ function FacilitiesAndKiosks() {
                             </Tooltip>
                           </TooltipProvider>
                           <Checkbox
-                            className="mr-0.5 2xl:mr-5 w-5 h-5"
+                            className="mr-0.5 w-5 h-5"
                             id={`kiosk-${kiosk.id}`}
                             checked={kiosksForUpdate.some(
                               (k) => k.id === kiosk.id
@@ -340,7 +342,7 @@ function FacilitiesAndKiosks() {
                         Produkter ({kiosk.products.length})
                       </div>
                       {kiosk.products && kiosk.products.length > 0 ? (
-                        <ul className="grid grid-cols-3 gap-3 mt-2">
+                        <ul className="grid grid-cols-1 sm:grid-cols-3 gap-3 mt-2">
                           {kiosk.products
                             .slice()
                             .sort((a, b) =>
@@ -360,7 +362,7 @@ function FacilitiesAndKiosks() {
                 </div>
               </div>
 
-              <div className="p-4 ">
+              <div className="p-4">
                 {facility.contactPersons?.length > 0 && (
                   <>
                     <div className="font-semibold text-lg ml-2 mb-2">
@@ -370,13 +372,13 @@ function FacilitiesAndKiosks() {
                       {facility.contactPersons?.map((contactPerson) => (
                         <div
                           key={contactPerson.id}
-                          className="p-4 border border-gray-200 rounded-md shadow flex justify-between dark:border-slate-500"
+                          className="p-4 border border-gray-200 rounded-md shadow flex flex-col md:flex-row justify-between dark:border-slate-500 mb-3"
                         >
-                          <p>
+                          <p className="mb-2 md:mb-0">
                             {contactPerson.name} - {contactPerson.role} -{" "}
                             {contactPerson.phone}
                           </p>
-                          <div className="flex justify-self-end gap-7 2xl:gap-3 ml-auto w-fit items-center">
+                          <div className="flex flex-wrap justify-end gap-4 ml-auto w-fit items-center">
                             <TooltipProvider>
                               <Tooltip>
                                 <TooltipTrigger>
